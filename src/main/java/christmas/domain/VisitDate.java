352e74119ddc2c7ec2816e0155ea421e570c9860
Hacanna42 <dtmdgus901@gmail.com>
2024-12-05 22:45:56 +0900

    feat(VisitDate): 방문 일자 객체 추가

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,31 @@
+package christmas.domain;
+
+import christmas.message.ErrorMessage;
+
+public class VisitDate {
+    private static final int CHRISTMAS_PROMOTION_MONTH = 12;
+
+    private final int month;
+    private final int day;
+
+    private VisitDate(int month, int day) {
+        this.month = month;
+        this.day = day;
+    }
+
+    public static VisitDate initFrom(String day) {
+        validate(day);
+        return new VisitDate(CHRISTMAS_PROMOTION_MONTH, Integer.parseInt(day));
+    }
+
+    private static void validate(String day) {
+        try {
+            int parsedDay = Integer.parseInt(day);
+            if (!(parsedDay >= 1 && parsedDay <= 31)) {
+                throw new IllegalArgumentException(ErrorMessage.DATE_INVALID.getMessage());
+            }
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ErrorMessage.DATE_INVALID.getMessage());
+        }
+    }
+}
