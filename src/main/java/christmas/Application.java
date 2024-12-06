2154a2d8c8439d79c6769a912b633f69e248616c
Hacanna42 <dtmdgus901@gmail.com>
2024-12-06 22:44:57 +0900

    feat(Application): Application 코드 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.controller.EventController;
+import christmas.service.EventService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        EventController eventController = new EventController(new EventService());
+        eventController.run();
     }
 }
